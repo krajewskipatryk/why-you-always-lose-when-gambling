cfce609bd21e2d91cdd746d170d6239a0da8bddc
patryk <krajewskipatryk@onet.pl>
2022-09-21 19:37:59 +0200

    Initial commit

---
+++ src/main/java/com/gamblingmethods/test/gambler/GamblerApplication.java
@@ -0,0 +1,13 @@
+package com.gamblingmethods.test.gambler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamblerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamblingmethods/test/gambler/GamblerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamblingmethods.test.gambler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamblerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
